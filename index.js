2c28caaaf57dbe24c129febf25370392aa25eb96
Luffy <swornimjoshi1122gmail.com>
2025-11-12 14:03:25 +0545

    fix index

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-console.log("Hello, World!");
 import express from 'express';
+import dotenv from 'dotenv';
 import { connectDB } from './src/configs/db.js';
+import authRoutes from './src/routes/authRoutes.js';
 
+dotenv.config();
 connectDB();
-const app = express();
-const PORT = process.env.PORT || 3000;
 
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(` Server running on port ${PORT}`);
 });
