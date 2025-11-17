ff890f57d9619d3d1565af102605f81e018340c3
Luffy <swornimjoshi1122gmail.com>
2025-11-17 15:15:12 +0545

    add trek routes to the main application

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './src/configs/db.js';
 import authRoutes from './src/routes/authRoutes.js';
+import trekRoutes from './src/routes/trekRoutes.js';
+
 
 dotenv.config();
 connectDB();
@@ -11,6 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/treks', trekRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
